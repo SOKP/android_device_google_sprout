75fdc53ee5f093eb9af8e6e466dba648dff2c6a9
AudioGod <adi.nth@outlook.com>
2015-03-13 22:37:56 +0530

    SOKPIfy
    Common Mandatory Changes needed Build SOKP

--- sokp.mk
+++ sokp.mk
@@ -2,12 +2,12 @@
 TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SOKP stuff.
+$(call inherit-product, vendor/sokp/config/common_full_phone.mk)
 
 $(call inherit-product, device/google/sprout/full_sprout.mk)
 
-PRODUCT_NAME := cm_sprout
+PRODUCT_NAME := sokp_sprout
 PRODUCT_DEVICE :=sprout
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
